0ea5f4551936b7a40f4e77c25955e8f8c80fc436
diego-venegas <diego.venegas.duran@gmail.com>
2022-10-24 18:32:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const Hapi = require('hapi');
 
 // logic port and machine
 const host = 'localhost';
-const PORT = process.env.PORT || 8000; 
+const port = process.env.PORT || 8000; 
 
 // Create Server
 const server = Hapi.Server({
     host: host,
-    PORT: PORT
+    port: port
 });
 
 // Initiate Server
 const init = async () => {
 
     await server.start();
-    console.log("Server up no porto: " + PORT);
+    console.log("Server up no porto: " + port);
 
 }
 //Define routes
